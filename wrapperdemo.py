339ae66095971def376dcaac29ed3ffb168b9a0f
carrotshub <1398141580@qq.com>
2018-12-23 15:50:27 +0800

    Initial commit

---
+++ wrapperdemo.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# 这是一个类装饰器
+class info(object):
+	def __init__(self,func):
+		self.func = func
+		# self.name = name
+
+	def __call__(self, *args, **kwargs):
+		print("[DEBUG] enter function {func}()".format(func=self.func.__name__))
+		print("func:\'{func}\' say : {something}".format(func=self.func.__name__, something=args[0]))
+		return self.func(*args, **kwargs)
+	@staticmethod
+	def get_id():
+		print("here is get your id!")
+
+
+@info
+def say(something):
+	print("say {}!".format(something))
+	def test():
+		print('here is a test dome!')
+	return test()
+
+
+say('hello world!')
+say('I am a test!')
